52ccded965784ae9472929364c050485627a1bf7
francesco <francescosanti91@gmail.com>
2021-11-09 14:49:32 +0100

    passi logici

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// Passi logici
+
+// ripeto fino a ottenere cinque numeri casuali univoci
+    // genero un numero casuale intero
+    // se il numero generato non è già parte della lista di numeri
+        // allora lo aggiungo alla lista
+// stampo in pagina la lista di numeri generata
+// dopo trenta secondi tolgo i numeri dalla pagina
+// ripeto quanto segue tante volte, quanto è lunga la lista di numeri da indovinare
+    // chiedo all'utente di scrivere uno dei numeri mostrati in pagina
+    // confronto il numero scritto dall'utente con tutti i numeri della lista
+        // se trovo corrispondenza
+            // allora inserisco il numero nella lista dei numeri indovinati
+// stampo in pagina i numeri indovinati dall'utente
